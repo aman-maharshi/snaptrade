18201eed42e1ff5d026ce9c5278a20c89f88e74a
aman-maharshi <amanm4harshi@gmail.com>
2025-09-26 20:11:57 +0530

    desktop header updats

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import React from "react"
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen text-gray-400 flex-col gap-4 md:gap-10 items-center sm:items-start">Home</div>
+  )
+}
+
+export default Home
