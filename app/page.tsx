57964fc5ddf03e6a5c2e1662d51c8a5c4d7038e4
aman-maharshi <amanm4harshi@gmail.com>
2025-09-26 19:45:22 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import React from "react"
+
+const Home = () => {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores similique architecto maxime nesciunt nulla
+        vitae sapiente quas amet animi commodi.
+      </p>
+      <Button>Login with SnapTrade</Button>
+    </div>
+  )
+}
+
+export default Home
