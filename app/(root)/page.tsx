cb6785643a4cae15671b27621310badf2ac2e6ab
aman-maharshi <amanm4harshi@gmail.com>
2025-09-28 23:37:17 +0530

    chart hook updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
-import { Button } from "@/components/ui/button"
+import TradingViewWidget from "@/components/TradingViewWidget"
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants"
 import React from "react"
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen text-neutral-400 flex-col gap-4 md:gap-10 items-center sm:items-start">Home</div>
+    <div className="flex min-h-screen text-neutral-400 flex-col gap-4 md:gap-10 items-center sm:items-start">
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            height={600}
+            className="custom-chart"
+          />
+        </div>
+      </section>
+    </div>
   )
 }
 
