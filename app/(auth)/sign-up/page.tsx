d534a6873eaaa821da6af261bf30c0022d7afa39
aman-maharshi <amanm4harshi@gmail.com>
2025-10-01 00:51:05 +0530

    signup form init

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,80 @@
+"use client"
+
 import React from "react"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form"
+import InputField from "@/components/forms/InputField"
 
 const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors, isSubmitting }
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "",
+      investmentGoals: "",
+      riskTolerance: "",
+      preferredIndustry: ""
+    },
+    mode: "onBlur"
+  })
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <>
       <h1 className="form-title">Sign Up & Personalize</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="John Doe"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full name is required", minLength: 2 }}
+        />
+
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="contact@email.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email name is required",
+            pattern: /^\w+@\w+\.\w+$/,
+            message: "Email address is required"
+          }}
+        />
+
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 8 }}
+        />
+
+        <button type="submit" className="yellow-btn w-full mt-5" disabled={isSubmitting}>
+          {isSubmitting ? "Creating account..." : "Start Your Investment Journey"}
+        </button>
+      </form>
     </>
   )
 }
